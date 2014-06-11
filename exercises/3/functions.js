7ccd7ee2f50864e152407cb991a2e002e49f7ad8
lewenhagen <lewenhagen@live.com>
2014-06-11 22:19:48 +0200

    started with lab 3, arrays

---
+++ exercises/3/functions.js
@@ -0,0 +1,47 @@
+/**
+ * Question 1
+ **/
+ function myFirstArray() {
+ 	var myArray = new Array(5);
+
+ 	for(var i = 0; i < myArray.length; i++) {
+ 		myArray[i] = i+1;
+ 	}
+ 	return myArray;
+ }
+
+/**
+ * Question 2
+ **/
+
+/**
+ * Question 3
+ **/
+
+/**
+ * Question 4
+ **/
+
+/**
+ * Question 5
+ **/
+
+/**
+ * Question 6
+ **/
+
+/**
+ * Question 7
+ **/
+
+/**
+ * Question 8
+ **/
+
+/**
+ * Question 9
+ **/
+
+/**
+ * Question 10
+ **/
