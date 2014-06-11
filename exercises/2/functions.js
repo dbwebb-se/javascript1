e1303f0493d78f54e230ca967a0a1e482b0fcd26
lewenhagen <lewenhagen@live.com>
2014-06-11 23:29:24 +0200

    more exercises on lab 3, fixed soft tab on earlier files

--- exercises/2/functions.js
+++ exercises/2/functions.js
@@ -2,165 +2,165 @@
  * Question 1
  **/
  function sumNumbers(a, b) {
- 	var sum = a + b;
- 	return sum;
+    var sum = a + b;
+    return sum;
  }
 
 /**
  * Question 2
  **/
  function productNumbers(a, b) {
- 	var sum = a * b;
- 	return sum;
+    var sum = a * b;
+    return sum;
  }
 
 /**
  * Question 3
  **/
  function sumRangeNumbers(lowest, highest) {
- 	var sum = 0;
- 	for(var i = lowest+1; i < highest; i++) {
- 		sum += i;
- 	}
- 	return sum;
+    var sum = 0;
+    for(var i = lowest+1; i < highest; i++) {
+        sum += i;
+    }
+    return sum;
  }
 
 /**
  * Question 4
  **/
  function stringPhrase(name) {
- 	var phrase = "Hi " + name + ", how are you?";
- 	return phrase;
+    var phrase = "Hi " + name + ", how are you?";
+    return phrase;
  }
 
 /**
  * Question 5
  **/
  function stringResponse(question) {
- 	var response = "Try again";
+    var response = "Try again";
 
- 	if(question === "How are you?") {
- 		response = "I am fine";
- 	}
- 	return response;
+    if(question === "How are you?") {
+        response = "I am fine";
+    }
+    return response;
  }
 
 /**
  * Question 6
  **/
  function checkIfHigher(a, b) {
- 	var isHigher = false;
+    var isHigher = false;
 
- 	if(a > b) {
- 		isHigher = true;
- 	}
- 	return isHigher;
+    if(a > b) {
+        isHigher = true;
+    }
+    return isHigher;
  }
 
 /**
  * Question 7
  **/
  function printRange(a, b) {
- 	var result = "";
- 	var i = 0;
+    var result = "";
+    var i = 0;
 
- 	if(a < b) {
- 		for(i = a; i < b+1; i++) {
- 			result += i + ",";
- 		}
- 	}
- 	else if(a > b) {
- 		for(i = a; i > b-1; i--) {
- 			result += i + ",";
- 		}
- 	}
- 	else {
- 		result = a;
- 	}
+    if(a < b) {
+        for(i = a; i < b+1; i++) {
+            result += i + ",";
+        }
+    }
+    else if(a > b) {
+        for(i = a; i > b-1; i--) {
+            result += i + ",";
+        }
+    }
+    else {
+        result = a;
+    }
 
- 	return result;
+    return result;
  }
 
 /**
  * Question 8
  **/
  function stringRepeat(text, number) {
- 	var result = '';
+    var result = '';
 
- 	for(var i = 0; i < number; i++) {
- 		result += text + ",";
- 	}
- 	return result;
+    for(var i = 0; i < number; i++) {
+        result += text + ",";
+    }
+    return result;
  }
 
 /**
  * Question 9
  **/
  function calculateInterest(money, years, interest) {
- 	var result = money, currentInterest = 0;
+    var result = money, currentInterest = 0;
 
- 	for(var i = 0; i < years; i++) {
- 		currentInterest = (result / 100) * interest;
- 		result += currentInterest; 
- 	}
- 	return result;
+    for(var i = 0; i < years; i++) {
+        currentInterest = (result / 100) * interest;
+        result += currentInterest; 
+    }
+    return result;
  }
 
 /**
  * Question 10
  **/
  function countNumbers(a, b) {
- 	var counter = 0;
+    var counter = 0;
 
- 	if(a < b) {
- 		for(var i = a+1; i < b; i++) {
- 			counter++;
- 		}
- 	}
- 	else if(a > b) {
- 		for(var i = a; a > b+1; a--) {
- 			counter++;
- 		}
- 	}
- 	return counter;
+    if(a < b) {
+        for(var i = a+1; i < b; i++) {
+            counter++;
+        }
+    }
+    else if(a > b) {
+        for(var i = a; a > b+1; a--) {
+            counter++;
+        }
+    }
+    return counter;
  }
 
 /**
  * Question 11
  **/
  function inRange(value) {
- 	var isInRange = false;
+    var isInRange = false;
 
- 	if(value > 10 && value < 20) {
- 		isInRange = true;
- 	}
- 	return isInRange;
+    if(value > 10 && value < 20) {
+        isInRange = true;
+    }
+    return isInRange;
  }
 
 /**
  * Question 12
  **/
  function fizzBuzz(start, stop) {
- 	var result = '';
+    var result = '';
 
- 	if(stop <= start) {
- 		result = 'Stop is lower than start or they are the same. Try again!';
- 	}
- 	else {
- 		for(var i = start; i < stop+1; i++) {
- 			if(i % 3 === 0 && i % 5 !== 0) {
- 				result += 'Fizz, ';
- 			}
- 			else if(i % 5 === 0 && i % 3 !== 0) {
- 				result += 'Buzz, ';
- 			}
- 			else if(i % 3 === 0 && i % 5 === 0) {
- 				result += 'Fizz Buzz, '
- 			}
- 			else {
- 				result += i + ', ';
- 			}
- 		}
- 	}
- 	return result;
+    if(stop <= start) {
+        result = 'Stop is lower than start or they are the same. Try again!';
+    }
+    else {
+        for(var i = start; i < stop+1; i++) {
+            if(i % 3 === 0 && i % 5 !== 0) {
+                result += 'Fizz, ';
+            }
+            else if(i % 5 === 0 && i % 3 !== 0) {
+                result += 'Buzz, ';
+            }
+            else if(i % 3 === 0 && i % 5 === 0) {
+                result += 'Fizz Buzz, '
+            }
+            else {
+                result += i + ', ';
+            }
+        }
+    }
+    return result;
  }
--- exercises/3/functions.js
+++ exercises/3/functions.js
@@ -2,41 +2,79 @@
  * Question 1
  **/
  function myFirstArray() {
- 	var myArray = new Array(5);
+    var myArray = new Array(5);
 
- 	for(var i = 0; i < myArray.length; i++) {
- 		myArray[i] = i+1;
- 	}
- 	return myArray;
+    for(var i = 0; i < myArray.length; i++) {
+        myArray[i] = i+1;
+    }
+    return myArray;
  }
 
 /**
  * Question 2
  **/
+ function arraySentence() {
+    var sentence = ["Hello!", "My", "name", "is", "John", "Doe"];
+    return sentence;
+ }
 
 /**
  * Question 3
  **/
+ function checkArray(testArray) {
+    return Array.isArray(testArray);
+ }
 
 /**
  * Question 4
  **/
+ function concArrays() {
+    var arrOne = ["This", "can", "be", "useful"];
+    var arrTwo = [1,2,3,4,5];
+    var arrThree = arrOne.concat(arrTwo);
+
+    return arrThree;
+ }
 
 /**
  * Question 5
  **/
+ function reverseArray(testArray) {
+    return testArray.reverse();
+ }
 
 /**
  * Question 6
  **/
+ function findIndex(testArray, val) {
+    return testArray.indexOf(val);
+ }
 
 /**
  * Question 7
  **/
+ function countArray(testArray) {
+    var counter = 0;
+
+    testArray.forEach(function(entry) {
+        counter++;
+    });
+
+    return counter;
+ }
 
 /**
  * Question 8
  **/
+ function changeArrayElements(testArray) {
+    var result = [];
+
+    testArray.forEach(function(entry) {
+        result.push(entry + 'x');
+    });
+    
+    return result;
+ }
 
 /**
  * Question 9
