ba75da774d3ea18954ae541694cf1a4add08ce51
lewenhagen <lewenhagen@live.com>
2014-06-12 15:26:21 +0200

    started with lab 4, (object literal)

---
+++ exercises/4/exercise.js
@@ -0,0 +1,95 @@
+/**
+ * Question 1
+ **/
+ // var person = {};
+
+/**
+ * Question 2
+ **/
+ document.writeln(firstAttributes().age);
+
+ document.writeln('<br>');
+
+/**
+ * Question 3
+ **/
+ document.writeln();
+
+ document.writeln('<br>');
+
+/**
+ * Question 4
+ **/
+ document.writeln();
+
+ document.writeln('<br>');
+
+/**
+ * Question 5
+ **/
+ document.writeln();
+
+ document.writeln('<br>');
+
+/**
+ * Question 6
+ **/
+ document.writeln();
+
+ document.writeln('<br>');
+
+/**
+ * Question 7
+ **/
+ document.writeln();
+
+ document.writeln('<br>');
+
+/**
+ * Question 8
+ **/
+ document.writeln();
+
+ document.writeln('<br>');
+
+/**
+ * Question 9
+ **/
+ document.writeln();
+
+ document.writeln('<br>');
+
+/**
+ * Question 10
+ **/
+ document.writeln();
+
+ document.writeln('<br>');
+
+/**
+ * Question 11
+ **/
+ document.writeln();
+
+ document.writeln('<br>');
+
+/**
+ * Question 12
+ **/
+ document.writeln();
+
+ document.writeln('<br>');
+
+/**
+ * Question 13
+ **/
+ document.writeln();
+
+ document.writeln('<br>');
+
+/**
+ * Question 14
+ **/
+ document.writeln();
+
+ document.writeln('<br>');
---
+++ exercises/4/functions.js
@@ -0,0 +1,68 @@
+/**
+ * Question 1
+ **/
+ var person = {};
+/**
+ * Question 2
+ **/
+ function firstAttributes() {
+    person.name = "John";
+    person.age = 30;
+    person.height = 180;
+    person.eyecolor = "blue";
+    return person;
+ }
+/**
+ * Question 3
+ **/
+ 
+/**
+ * Question 4
+ **/
+ 
+/**
+ * Question 5
+ **/
+ 
+/**
+ * Question 6
+ **/
+ 
+/**
+ * Question 7
+ **/
+ 
+/**
+ * Question 8
+ **/
+ 
+
+/**
+ * Question 9
+ **/
+ 
+
+/**
+ * Question 10
+ **/
+ 
+
+/**
+ * Question 11
+ **/
+ 
+
+/**
+ * Question 12
+ **/
+ 
+
+/**
+ * Question 13
+ **/
+ 
+
+/**
+ * Question 14
+ **/
+ 
