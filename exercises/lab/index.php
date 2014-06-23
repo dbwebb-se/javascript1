214976b6918812d6779d9375c1665fe0477fb06a
Mikael Roos <mikael.t.h.roos@gmail.com>
2014-06-23 15:57:44 +0200

    create lab

--- exercises/lab/index.php
+++ exercises/lab/index.php
@@ -82,6 +82,9 @@ EOD;
 <meta charset="utf-8">
 <title>Lab generation</title>
 <link rel="stylesheet" href="style/style.css">
+<style>
+<?=file_get_contents("style/style.css")?>
+</style>
 </head>
 <body>
 
--- exercises/lab/view/lab_tpl.php
+++ exercises/lab/view/lab_tpl.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <title><?=$title?></title>
     <link rel="stylesheet" href="style/style.css">
+<style>
+<?=file_get_contents("style/style.css")?>
+</style>
 </head>
 <body>
 
