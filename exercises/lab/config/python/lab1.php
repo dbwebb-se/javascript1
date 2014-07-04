d184cdab86a03662d95893eaaa2bb268cc2eff74
Lew <lewenhagen@live.com>
2014-07-04 23:28:39 +0200

    lab 1 more excercises

--- exercises/lab/config/python/lab1.php
+++ exercises/lab/config/python/lab1.php
@@ -8,9 +8,12 @@ $wordSerie1 = ["storage", "memory", "device", "syntax", "computer", "error", "pr
 $wordRand1 = 7; // 0-9 for the word array
 $wordRand2 = 3; // 0-9 for the word array
 
-$intOne = 78; // 20-999
-$intTwo = 123;  // 20-999
-$intThree = 45; // 20-99
+$numSerie1 = [123,22.43,342,2,54.22,2.564,43,45.33,435,2.33];
+
+$intOne = 78; // 20-499
+$intTwo = 123;  // 20-499
+$intThree = 45; // 1-100
+$bigInt = 654; // 500-999
 $floatOne = 5.77; // FLOATS! 10.0-999.99
 $floatTwo = 654.34; // FLOATS! 10.0-999.99 
 
@@ -43,7 +46,7 @@ return [
 "title" => "Integers, floats and basic arithmetics",
 
 "intro" => "
-<p>The foundation of variables, numbers and basic arithmetic.</p>
+<p>The foundation of numbers and basic arithmetic.</p>
 ",
 
 "shuffle" => false,
@@ -204,6 +207,25 @@ return [
 
 
 
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => '
+<p>Print out the result of ' . $intOne . ' modulo (%) ' . $wordRand1 . '.
+</p>
+',
+
+"answer" => function () use ($intOne, $wordRand1) {
+
+    return $intOne%$wordRand1;
+},
+
+],
+
+
+
 /**
  * Closing up this section.
  */
@@ -216,10 +238,10 @@ return [
  * New section of exercises.
  */
 [
-"title" => "Variables, expressions and statements",
+"title" => "Conditions, exceptions, booleans, if, else and elif",
 
 "intro" => "
-<p>some intro text about this...
+<p>loops n stuff
 </p>
 ",
 
@@ -235,16 +257,181 @@ return [
 [
 
 "text" => '
-<p>Create a 
+<p>Print out the boolean value of: ' . $intOne . ' < ' . $intTwo . '.
 </p>
 ',
 
-"answer" => function () {
+"answer" => function () use ($intOne, $intTwo) {
 
-    
+	return $intOne < $intTwo;  
 },
 
 ],
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => '
+<p>Print out the boolean value of: ' . $intOne . ' > ' . $intTwo . '.
+</p>
+',
+
+"answer" => function () use ($intOne, $intTwo) {
+
+	return $intOne > $intTwo;
+},
+
+],
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => '
+<p>Print out the boolean value of: ' . $intOne . ' == ' . $intTwo . '.
+</p>
+',
+
+"answer" => function () use ($intOne, $intTwo) {
+
+	return $intOne == $intTwo;
+},
+
+],
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => '
+<p>Create an if statement to see if ' . ($intOne*2) . ' is bigger than ' . ($intTwo*2) . '. If it is true, print out "yes", else print out "no".
+</p>
+',
+
+"answer" => function () use ($intOne, $intTwo) {
+
+	$result = "no";
+	if(($intOne*2) > ($intTwo*2)) {
+		$result = "yes";
+	}
+	return $result; 
+},
+
+],
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => '
+<p>Create an if statement to see if ' . $intOne . ' is bigger than ' . $intTwo . ' and smaller than ' . $bigInt . '. If it is true, print out "yes", else print out "no".
+</p>
+',
+
+"answer" => function () use ($intOne, $intTwo, $bigInt) {
+
+	$result = "no";
+	if($intOne > $intTwo && $intOne < $bigInt) {
+		$result = "yes";
+	}
+	return $result; 
+},
+
+],
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => '
+<p>Create an elif statement that checks in what range a value belongs to. The ranges should be: below 10, 10 to 25, 26 to 50, 51 to 75 and 76 to 100. If the number is 24, you should print out: "10 to 25" and if the number is 5, print out "below 10". Use the number ' . $intThree . ' and print out the corresponding result. It should work with any number between 0 and 100.
+</p>
+',
+
+"answer" => function () use ($intThree) {
+
+	$result = "";
+	if($intThree < 10) {
+		$result = "below 10";
+	}
+	if($intThree > 9 && $intThree <= 25) {
+		$result = "10 to 25";
+	}
+	if($intThree > 25 && $intThree <= 50) {
+		$result = "26 to 50";
+	}
+	if($intThree > 50 && $intThree <= 75) {
+		$result = "51 to 75";
+	}
+	if($intThree > 75 && $intThree <= 100) {
+		$result = "75 to 100";
+	}
+	
+	return $result; 
+},
+
+],
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => '
+<p>Create a try/except statement that should print whether a value is a float or an integer. If the value is an integer, then print "integer". If the value is a float, print "float". Otherwise print "neither". Use the value: ' . $numSerie1[$wordRand2] . ' when you submit your answer.
+</p>
+',
+
+"answer" => function () use ($numSerie1, $wordRand2) {
+
+	$result = "neither";
+	if(is_int($numSerie1[$wordRand2])) {
+		$result = "integer";
+	}
+	if(is_float($numSerie1[$wordRand2])) {
+		$result = "float";
+	}
+	return $result;
+},
+
+],
+
+
+/**
+ * Closing up this section.
+ */
+], // EOF questions
+], // EOF section
+
+
+
+/** ===================================================================================
+ * New section of exercises.
+ */
+[
+"title" => "Functions",
+
+"intro" => "
+<p>functions n stuff
+</p>
+",
+
+"shuffle" => false,
+
+"questions" => [
+
 
 
 /** -----------------------------------------------------------------------------------
@@ -259,7 +446,51 @@ return [
 
 "answer" => function () {
 
-    
+	
+},
+
+],
+
+
+
+/**
+ * Closing up this section.
+ */
+], // EOF questions
+], // EOF section
+
+
+
+/** ===================================================================================
+ * New section of exercises.
+ */
+[
+"title" => "Iteration and loops",
+
+"intro" => "
+<p>functions n stuff
+</p>
+",
+
+"shuffle" => false,
+
+"questions" => [
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => '
+<p>
+</p>
+',
+
+"answer" => function () {
+
+	
 },
 
 ],
