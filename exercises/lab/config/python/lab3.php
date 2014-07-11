84af1353b024f31262a24db6b828ceb0ff0c03c6
Lew <lewenhagen@live.com>
2014-07-11 14:12:03 +0200

    lab 3 almost done

--- exercises/lab/config/python/lab3.php
+++ exercises/lab/config/python/lab3.php
@@ -3,36 +3,32 @@
 /**
  * Titel and introduction to the lab.
  */
-
-$firstWord = "JavaScript";
-$secondWord = "rocks!";
-
-$numberOne = 10; // 20-999
-$numberTwo = 6;  // 20-999
-/*
-$numberThree = 56; // 20-999
-$numberFour = 348; // 110 - 450
-$floatOne = 12.55; // 100-999
-$floatTwo = 5.22; // 100-999 
-$lowNr = 15; // 0 - 100
-$highNr = 625; // 500 - 999
-$smallNr = 2; // 1-5
-$smallRandNr = 3; // 0-9
-$caseNrs = [1, 2, 3, 4, 5];
-$smallestRandNr = 1; // 0-2
-$aYear = 2014;
-$aMonth = 'Aug';
-$aDay = 5;
-$aDate = "$aMonth $aDay, $aYear";
-
-
-$serie1 = [347, -221, 54, 435];  // 100-999
-$wordSerie1 = ['bulldog', 'rabbit', 'chicken', 'mouse', 'horse', 'camel', 'crocodile', 'werewolf', 'reindeer', 'elephant'];
-$wordSerie2 = ['guitar', 'violin', 'drums'];
-$sentenceSerie1 = ['I am in a glass case of emotion', 'If peeing your pants is cool, consider me Miles Davis', 'Do you want to hear the most annoying sound in the world?', 'Thank you very little', 'Tigers love pepper, they hate cinnamon.', 'I wake up in the morning and I piss excellence', 'I think most Scottish cuisine is based on a dare', 'I do not know, I mostly just hurt people.', 'I aim to misbehave.', ' I wish monkeys could Skype'];
-$someIntText = $numberOne . ".$numberTwo " . $wordSerie1[$smallRandNr]; 
-*/
-
+$words = ["Finland", "Sweden", "Norway", "Denmark", "Iceland"];
+$listWords = [
+["table", "wall", "desk", "chair", "floor"],
+["wasp", "fly", "butterfly", "bumblebee", "mosquito"],
+["lion", "tiger", "ozelot", "bobcat", "cougar"],
+["Dafoe", "Sheen", "Berenger", "Depp", "Whitaker"],
+["flute", "guitar", "drums", "piano", "bass"]
+];
+$listSent = [
+"The?sun?is?shining",
+"The?snow?is?falling",
+"The?rain?is?pouring",
+"The?wind?is?blowing",
+"The?grass?is?growing"
+];
+$listNumbers = [
+[98,5,12,369,1],
+[123,4,125,69,155],
+[67,50,2,39,15],
+[567,23,12,36,7],
+[45,22,2,498,78]
+];
+$r1 = 1; // 0-4
+$r2 = 2; // 0-4
+$r3 = 3; // 0-4
+$r4 = 4; // 0-4
 
 
 return [
@@ -45,7 +41,7 @@ return [
 "title" => "Lab 3 - python",
 
 "intro" => "
-<p>If you need to peek at examples or just want to know more, take a look at the references at MDN's (Mozilla Developers Network) page: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference. Here you will find everything this lab will go through and much more.
+<p>Lists
 </p>
 ",
 
@@ -58,10 +54,11 @@ return [
  * New section of exercises.
  */
 [
-"title" => "Integers, floats and variables",
+"title" => "SECTION 1",
 
 "intro" => "
-<p>The foundation of variables, numbers, strings and basic arithmetic.</p>
+<p>????
+</p>
 ",
 
 "shuffle" => false,
@@ -76,13 +73,230 @@ return [
 [
 
 "text" => '
-<p>Create a variable called "numberOne" and give it the value ' . $numberOne . '. Create another variable called "numberTwo" and give it the value ' . $numberTwo . '. Create a third variable called "sum" and assign to it the sum of the first two variables. Print out the result.</p>
+<p>Concatinate the two lists [' . $listWords[$r1][0] . ', ' . $listWords[$r1][1] . '] and [' . $listWords[$r1][2] . ', ' . $listWords[$r1][3] . ']. Answer with your list. 
+</p>
 ',
 
-"answer" => function () use ($numberOne, $numberTwo) {
+"answer" => function () use ($listwords, $r1) {
 
-    $sum = $numberOne + $numberTwo;
-    return $sum;
+	
+},
+
+],
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => '
+<p>Use the list [' . implode(",", $listWords[$r2]) . ']. Update it with the words: "' . $words[$r2] . '" and "' . $words[$r4] . '" as the second and third element. Answer with the modified list.
+</p>
+',
+
+"answer" => function () use ($listwords, $r2, $r4, $words) {
+
+	
+},
+
+],
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => '
+<p>Use the list [' . implode(",", $listWords[$r3]) . ']. Replace the third word with: "' . $words[$r3] . '". Answer with the modified list.
+</p>
+',
+
+"answer" => function () use ($listwords, $r3, $words) {
+
+	
+},
+
+],
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => '
+<p>Use the list [' . implode(",", $listWords[0]) . ']. Extend it with the list: [' . implode(",", $listWords[1]) . ']. Answer with the modified list.
+</p>
+',
+
+"answer" => function () use ($listwords) {
+//0 and 1
+	
+},
+
+],
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => '
+<p>Sort the list [' . implode(",", $listWords[$r3]) . ']. Answer with the sorted list.
+</p>
+',
+
+"answer" => function () use ($listwords, $r3) {
+//0 and 1
+	
+},
+
+],
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => '
+<p>Use pop() to delete the second and the last element in the list: [' . implode(",", $listWords[$r2]) . ']. Answer with the modified list.
+</p>
+',
+
+"answer" => function () use ($listwords, $r2) {
+//0 and 1
+	
+},
+
+],
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => '
+<p>Use remove() to delete the word: "' . $listWords[$r3][$r3] . '" from the list: [' . implode(",", $listWords[$r3]) . ']. Answer with the modified list.
+</p>
+',
+
+"answer" => function () use ($listwords, $r3) {
+//0 and 1
+	
+},
+
+],
+
+
+
+/**
+ * Closing up this section.
+ */
+], // EOF questions
+], // EOF section
+
+
+
+/** ===================================================================================
+ * New section of exercises.
+ */
+[
+"title" => "Built-in list functions",
+
+"intro" => "
+<p>????
+</p>
+",
+
+"shuffle" => false,
+
+"questions" => [
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => '
+<p>Use a built-in function to sum all numbers in the list: [' . implode(",",$listNumbers[$r1]) . ']. Answer with the result as an integer.
+</p>
+',
+
+"answer" => function () use ($listNumbers, $r1) {
+
+    return array_sum($listNumbers[$r1]);
+},
+
+],
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => '
+<p>Use a built-in function to get the average value of the list: [' . implode(",",$listNumbers[$r3]) . ']. Answer with the result as a float.
+</p>
+',
+
+"answer" => function () use ($listNumbers, $r3) {
+
+    return floatval(array_sum($listNumbers[$r3])/count($listNumbers[$r3]));
+},
+
+],
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => '
+<p>Use a built-in function to get the lowest number in the list: [' . implode(",",$listNumbers[$r4]) . ']. Answer with the result as a integer.
+</p>
+',
+
+"answer" => function () use ($listNumbers, $r4) {
+
+    return min($listNumbers[$r4]);
+},
+
+],
+
+
+
+/** -----------------------------------------------------------------------------------
+ * A question.
+ */
+[
+
+"text" => '
+<p>Use the built-in functions split() and join() to fix this string: "' . $listSent[$r2] . '" into a proper one, (without "?"). Answer with the fixed string.
+</p>
+',
+
+"answer" => function () use ($listSent, $r2) {
+
+    $a = explode("?", $listSent[$r2]);
+    $b = join($a, " ");
+    return $b;
 },
 
 ],
@@ -107,4 +321,4 @@ return [
 /**
  * Closing up this lab.
  */
-]; // EOF the enritre lab
+]; // EOF the entire lab
