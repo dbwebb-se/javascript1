555c6919ad8ebc9728870deec9f99e36c4ad3a76
Mikael Roos <mikael.t.h.roos@gmail.com>
2014-08-15 09:19:42 +0200

    extending example

--- example/lawnmover/js/main.js
+++ example/lawnmover/js/main.js
@@ -14,7 +14,17 @@ window.lawnMower = (function(){
         lawnArea        = document.getElementById("lawnArea"),
         lawnMower       = document.getElementById("lawnMower"),
         lawnMowerWidth  = lawnMower.offsetWidth,
-        lawnMowerHeight = lawnMower.offsetHeight;
+        lawnMowerHeight = lawnMower.offsetHeight,
+        lawnMowerPosX   = 0,
+        lawnMowerPosY   = 0,
+        lawnMowerSpeed  = 30,
+        lawnMowerDirection = 0, // degrees
+        lawnMowerGear   = 0, 
+        lawnMowerEngine = false,
+        lawnMowerRunState = false,
+        lastGameTick    = null,
+        td              = null,
+        gameLoopId      = null;
 
     console.log("Browser area are width x height: " + windowWidth + " x " + windowHeight);
 
@@ -60,14 +70,13 @@ window.lawnMower = (function(){
         console.log("Placing the lawn mower with its center at position x, y: " + x + ", " + y);
 
         // Respect the size of the lawn mower when placing it
-        posX = Math.round(x - lawnMowerWidth / 2);
-        posY = Math.round(y - lawnMowerHeight / 2);
+        lawnMowerPosX = Math.round(x - lawnMowerWidth / 2);
+        lawnMowerPosY = Math.round(y - lawnMowerHeight / 2);
 
         console.log("Lawn mower has dimensions of width x height: " + lawnMowerWidth + " x " + lawnMowerHeight);
-        console.log("Placing the lawn mower at left, top: " + posX + ", " + posY);
-
-        lawnMower.style.left = posX + "px";
-        lawnMower.style.top  = posY + "px";
+        console.log("Placing the lawn mower at left, top: " + lawnMowerPosX + ", " + lawnMowerPosY);
+        
+        lawnMowerMove(lawnMowerPosX, lawnMowerPosY);
 
         console.log("Done.")
     };
@@ -75,14 +84,241 @@ window.lawnMower = (function(){
 
 
     /**
+     * Set speed for lawn mower, if argument is sent. Return current speed.
+     */
+    var setSpeed = function (speed) {
+        if (speed >= 1 && speed <= 100) {
+            lawnMowerSpeed = speed;
+            console.log("Set speed to " + lawnMowerSpeed)
+        }
+
+        console.log("Current speed is " + lawnMowerSpeed)
+        return lawnMowerSpeed;
+    };
+
+
+
+    /**
+     * Set direction for lawn mower, where its front points, if argument is present. 
+     * Return current value.
+     */
+    var setDirection = function (direction) {
+        if (direction >= -360 && direction <= 360) {
+            lawnMowerDirection = direction;
+            lawnMower.style.transform = "rotate(" + lawnMowerDirection + "deg)";
+            console.log("Set direction to " + lawnMowerDirection)
+        }
+
+        console.log("Current direction is " + lawnMowerDirection)
+        return lawnMowerDirection;
+    };
+
+
+
+    /**
+     * Set the gear 0, back=-1 or forward=1. 
+     */
+    var setGear = function (gear) {
+        console.log("Gear is:" + lawnMowerGear);
+
+        if (gear === -1 || gear === 0 || gear === 1) {
+            lawnMowerGear = gear;
+            console.log("Setting gear to:" + lawnMowerGear);
+        }
+
+        return lawnMowerGear;
+    };
+
+
+
+    /**
+     * Start engine. 
+     */
+    var startEngine = function () {
+        lawnMowerEngine = true;
+        setGear(0);
+        console.log("Started engine, brumm, brumm")
+    };
+
+
+
+    /**
+     * Start engine. 
+     */
+    var stopEngine = function () {
+        lawnMowerEngine = false;
+        setGear(0);
+        console.log("Stopped engine")
+    };
+
+
+
+    /**
+     * Move forward. 
+     */
+    var moveForward = function (td) {
+        var x = lawnMowerPosX,
+            y = lawnMowerPosY,
+            radians = lawnMowerDirection * Math.PI / 180;
+
+        if (!lawnMowerEngine) {
+            console.log("Can not move, the engine is not started.")
+            return;
+        }
+
+        lawnMowerPosX += lawnMowerSpeed * Math.cos(radians) * td;
+        lawnMowerPosY += lawnMowerSpeed * Math.sin(radians) * td;
+
+        lawnMowerMove(lawnMowerPosX, lawnMowerPosY);
+
+        //console.log("Moved forward from " + x + ", " + y + " to " + lawnMowerPosX + ", " + lawnMowerPosY);
+    };
+
+
+
+    /**
+     * Move backward. 
+     */
+    var moveBackward = function (td) {
+        var x = lawnMowerPosX,
+            y = lawnMowerPosY,
+            radians = lawnMowerDirection * Math.PI / 180;
+
+        if (!lawnMowerEngine) {
+            console.log("Can not move, the engine is not started.")
+            return;
+        }
+        
+        lawnMowerPosX -= lawnMowerSpeed * Math.cos(radians) * td;
+        lawnMowerPosY -= lawnMowerSpeed * Math.sin(radians) * td;
+
+        lawnMowerMove(lawnMowerPosX, lawnMowerPosY);
+
+        //console.log("Moved backward from " + x + ", " + y + " to " + lawnMowerPosX + ", " + lawnMowerPosY);
+    };
+
+
+
+    /**
+     * Move lawn mower to current position. 
+     */
+    var lawnMowerMove = function () {
+        lawnMower.style.left = lawnMowerPosX + "px";
+        lawnMower.style.top  = lawnMowerPosY + "px";
+    };
+
+
+
+    /**
+     * Current state of the lawn mower. 
+     */
+    var lawnMowerStatus = function () {
+        console.log("Engine is on: " + lawnMowerEngine);
+        console.log("Run state is on: " + lawnMowerRunState);
+        console.log("Speed is: " + lawnMowerSpeed);
+        console.log("Direction is: " + lawnMowerDirection);
+        console.log("Position is x,y: " + lawnMowerPosX + ", " + lawnMowerPosX);
+        console.log("Gear is: " + lawnMowerGear);
+    };
+
+
+
+    /**
+     * Make thw lawn mower go by itself. 
+     */
+    var lawnMowerRun = function (run) {
+        if (!lawnMowerEngine) {
+            console.log("Can not run, the engine is not started.")
+            return;
+        }
+        
+        lawnMowerRunState = (run === undefined) ? !lawnMowerRunState : run;
+
+        console.log("Run state is " + lawnMowerRunState);
+
+        if (lawnMowerRunState) {
+            gameLoopId = window.requestAnimationFrame(gameLoop);
+            console.log("Gameloop started");
+        } else {
+            console.log("Stopping runstate");
+        }
+    };
+
+
+
+    /**
+     * The game loop. 
+     */
+    var gameLoop = function () {
+        var now = Date.now();
+        td = (now - (lastGameTick || now)) / 1000;
+        lastGameTick = now;
+
+        if (lawnMowerEngine === false || lawnMowerRunState === false) {
+            window.cancelAnimationFrame(gameLoopId);
+            console.log("Engine/Runstate was stopped, gameloop is cancelled")
+            return;
+        }
+
+        window.requestAnimationFrame(gameLoop);
+        update(td);
+    };
+
+
+
+    /**
+     * Update one game tick. 
+     */
+    var update = function (td) {
+
+        if (lawnMowerGear === 1) {
+            moveForward(td);
+        } else if (lawnMowerGear === -1) {
+            moveBackward(td);
+        }
+    };
+
+
+
+    /**
+     * Click to make lawn mower go in a specific direction. 
+     */
+    lawnArea.addEventListener("click", function (event) {
+        var theta;
+
+        console.log("Clicked with the mouse at x,y: " + event.clientX + ", " + event.clientY);
+        theta = Math.atan2(event.clientY - lawnMowerPosY, event.clientX - lawnMowerPosX);
+        setDirection(theta * 180 / Math.PI);
+    });
+
+
+    // keydown
+    // klippa gräset
+    // längd gått
+    // tid passerad
+    // procent klippt
+    // gräns
+    // gränsavkännare
+    
+
+    /**
      * All done, return object exposing functions and variables to the outside world.
      */ 
     console.log('Lawn mover is ready!');
 
     return {
-        "element":  lawnMower,
-        "init":     initLawn,
-        "place":    placeLawnMower
+        "element":      lawnMower,
+        "speed":        setSpeed,
+        "direction":    setDirection,
+        "init":         initLawn,
+        "place":        placeLawnMower,
+        "start":        startEngine,
+        "stop":         stopEngine,
+        "forward":      moveForward,
+        "back":         moveBackward,
+        "run":          lawnMowerRun,
+        "gear":         setGear,
+        "status":       lawnMowerStatus
     }
 })();
 
@@ -96,5 +332,7 @@ window.lawnMower = (function(){
 
 lawnMower.init();
 lawnMower.place();
+lawnMower.start();
+//lawnMower.run();
 
-
+m = lawnMower;
