8235a9185f990edec472b9a7f359d5d937f79136
Kenneth Lewenhagen <lewenhagen@live.com>
2018-10-22 10:29:52 +0200

    removed trailing space

--- example/guide/js1/getelementsbyid/js/main.js
+++ example/guide/js1/getelementsbyid/js/main.js
@@ -4,7 +4,7 @@
     var myContent = document.getElementById('content');
 
     myContent.innerHTML = '<h3>This is a template!</h3>';
-    
+
     console.log(myContent);
 
     window.console.log('Sandbox is ready!');
