b5c44121ab750464a65ab5606d1fcba756c4aef6
Kenneth Lewenhagen <lewenhagen@live.com>
2018-10-22 10:08:21 +0200

    added guide to js1

---
+++ example/guide/js1/getelementsbyid/js/main.js
@@ -0,0 +1,11 @@
+(function () {
+    'use strict';
+
+    var myContent = document.getElementById('content');
+
+    myContent.innerHTML = '<h3>This is a template!</h3>';
+    
+    console.log(myContent);
+
+    window.console.log('Sandbox is ready!');
+})();
