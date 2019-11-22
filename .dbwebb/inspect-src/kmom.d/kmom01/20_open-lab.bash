ee8b8b40a2e70db1d7cf55348aa229015759c907
Mikael Roos <mikael.t.h.roos@gmail.com>
2019-11-22 15:09:40 +0100

    Remove text and scripts from .dbwebb

--- .dbwebb/inspect-src/kmom.d/kmom01/20_open-lab.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Open lab1
-printf "$REDOVISA_HTTP_PREFIX/~$acronym/dbwebb-kurser/$COURSE/me/kmom01/lab1/answer.html\n" 2>&1
-
-eval "$BROWSER" "$REDOVISA_HTTP_PREFIX/~$acronym/dbwebb-kurser/$COURSE/me/kmom01/lab1/answer.html" &
--- .dbwebb/inspect-src/kmom.d/kmom01/pre.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Open me/kmom01/lab1
-url="$REDOVISA_HTTP_PREFIX/~$acronym/dbwebb-kurser/$COURSE/me/kmom01/lab1/answer.html"
-printf "$url\n" 2>&1
-eval "$BROWSER" "$url" &
